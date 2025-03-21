f1f662a2e562bdcfa8a7b196c9651fa7e28c2da8
Talal Alotibi <tensazangets9@gmail.com>
2025-03-21 21:44:07 +0300

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1062,7 +1062,7 @@ function App() {
   if (isLoading && !records.length) {
     return (
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-indigo-500"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-indigo-500 "></div>
       </div>
     );
   }
